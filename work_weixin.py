e1b5cda6c15003dc928747e2ef782b1ec4e3e108
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2021-06-03 21:15:10 +0800

    Add files via upload

---
+++ work_weixin.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+'''
+Created on 2021年6月2日
+@author: yuejing
+'''
+# https://www.cnblogs.com/rancher-maomao/p/10860069.html
+
+import time
+import requests
+import json
+
+class WeChat:
+    def __init__(self):
+    	#企业ID，在管理后台获取
+        self.CORPID = 'XXX'  
+        #自建应用的Secret，每个自建应用里都有单独的secret
+        self.CORPSECRET = 'XXX'
+        #应用ID，在后台应用中获取
+        self.AGENTID = 'XXX'  
+        #接收者用户名,多个用户用|分割
+        self.TOUSER = "YueJing|LiFei"  
+
+    def send_message(self, Content):
+    	#获取access_token
+        url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+        values = {'corpid': self.CORPID,
+                  'corpsecret': self.CORPSECRET }
+        req = requests.post(url, params=values)
+        data = json.loads(req.text)
+        access_token=data["access_token"]
+        #发送企业微信消息
+        send_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token
+        send_values = {
+            "touser": self.TOUSER,
+            "msgtype": "text",
+            "agentid": self.AGENTID,
+            "text": {"content": Content},
+            "safe": "0" }
+        send_msges=(bytes(json.dumps(send_values), 'utf-8'))
+        respone = requests.post(send_url, send_msges)
+        respone = respone.json() 
+        print(respone["errmsg"])
+        return respone["errmsg"]
+
+if __name__ == '__main__':
+    wx = WeChat()
+    wx.send_message("【标题】\n通知正文")
