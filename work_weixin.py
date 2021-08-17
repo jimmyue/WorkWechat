5c7a6715fab44b84dee6c308c9cd72c0c4dc965c
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2021-08-17 14:23:31 +0800

    Add files via upload

--- work_weixin.py
+++ work_weixin.py
@@ -4,15 +4,14 @@
 Created on 2021年6月2日
 @author: yuejing
 '''
-# https://www.cnblogs.com/rancher-maomao/p/10860069.html
-
+import os
 import time
 import requests
 import json
 
 class WeChat:
     def __init__(self):
-    	#企业ID，在管理后台获取
+        #企业ID，在管理后台获取
         self.CORPID = 'XXX'  
         #自建应用的Secret，每个自建应用里都有单独的secret
         self.CORPSECRET = 'XXX'
@@ -21,21 +20,27 @@ class WeChat:
         #接收者用户名,多个用户用|分割
         self.TOUSER = "@all"  
         #接收部门ID，多个部门用|分割
-        self.TOPARTY="2"
+        self.TOPARTY='2'
 
-    def send_message(self, Content):
-    	#获取access_token
+    #企业微信接口 https://work.weixin.qq.com/api/doc/90000/90135/91039
+    def get_token(self):
         url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
         values = {'corpid': self.CORPID,
                   'corpsecret': self.CORPSECRET }
         req = requests.post(url, params=values)
-        data = json.loads(req.text)
+        data = req.json() 
         access_token=data["access_token"]
-        #发送企业微信消息
+        #写入access_token
+        fo = open('access_token.txt', "w")
+        fo.write(access_token)
+        fo.close()
+
+    #企业微信接口 https://work.weixin.qq.com/api/doc/90000/90135/90236
+    def send_msg(self,access_token,Content):
         send_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token
         send_values = {
             "touser": self.TOUSER,
-            "toparty":self.TOPARTY,
+            "toparty": self.TOPARTY,
             "msgtype": "text",
             "agentid": self.AGENTID,
             "text": {"content": Content},
@@ -46,6 +51,28 @@ class WeChat:
         print(respone["errmsg"])
         return respone["errmsg"]
 
+    #企业微信规定gettoken接口不能频繁请求
+    def send_message(self, Content):
+        try:
+            #不存在access_token.txt则获取access_token生成
+            if not os.path.exists('access_token.txt'):
+                self.get_token()
+            #循环获取有效access_token，直到发送消息
+            result='do'
+            while result != 'ok':
+                #读取access_token
+                fo = open('access_token.txt', "r+")
+                access_token = fo.read()
+                fo.close()
+                #发送消息
+                result=self.send_msg(access_token,Content)
+                if result == 'ok':
+                    break
+                #获取新的access_token
+                self.get_token()
+        except Exception as e:
+            print(str(e)) 
+
 if __name__ == '__main__':
     wx = WeChat()
-    wx.send_message("【标题】\n通知正文")
+    wx.send_message("【标题测试】\n通知正文测试")
