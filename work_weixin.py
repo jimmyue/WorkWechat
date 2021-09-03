3c07ae2e8836986ca3c1f281c543b8ae35aeaa17
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2021-09-03 17:26:00 +0800

    Update work_weixin.py

--- work_weixin.py
+++ work_weixin.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 '''
-Created on 2021年6月2日
+Created on 2021年9月3日
 @author: yuejing
 '''
 import os
-import time
 import requests
 import json
+import pymysql
+import datetime
 
 class WeChat:
     def __init__(self):
-        #企业ID，在管理后台获取
-        self.CORPID = 'XXX'  
+    	#企业ID，在管理后台获取
+        self.CORPID = 'ww402caf66f8c12721'  
         #自建应用的Secret，每个自建应用里都有单独的secret
-        self.CORPSECRET = 'XXX'
+        self.CORPSECRET = 'Dyz8H4VRZEusRWkiWg37HSx7S6gdAFwe1UeH0h3Cg6Q'
         #应用ID，在后台应用中获取
-        self.AGENTID = 'XXX'  
+        self.AGENTID = '1000002'  
         #接收者用户名,多个用户用|分割
         self.TOUSER = "@all"  
         #接收部门ID，多个部门用|分割
-        self.TOPARTY='2'
+        self.TOPARTY='1'
 
     #企业微信接口 https://work.weixin.qq.com/api/doc/90000/90135/91039
     def get_token(self):
@@ -73,6 +74,25 @@ class WeChat:
         except Exception as e:
             print(str(e)) 
 
+def run_compare():
+	conn=pymysql.connect(host='XXXX',port=3306,user='root',passwd='123456',db='jimmy',use_unicode=True, charset="utf8")
+	cur = conn.cursor()
+	#查询数据库
+	cur.execute("select DATE_FORMAT(CDATE,'%Y-%m-%d')+INTERVAL 27 DAY real_time from weixin where type='MC' and CDATE=(SELECT max(CDATE) from weixin)")
+	result = cur.fetchone()[0]
+	print(result,"提醒！")
+	#插入数据库
+	today = datetime.date.today()
+	if result==str(today):
+		#微信提醒
+		wx = WeChat()
+		wx.send_message("【姨妈提醒】\n宝宝，明天要大姨妈了，注意防护呀，爱你~")
+		#插入时间
+		cur.execute("insert into weixin select null,'MC',SYSDATE()+INTERVAL 1 DAY")
+		conn.commit()
+	#关闭
+	cur.close()
+	conn.close()
+
 if __name__ == '__main__':
-    wx = WeChat()
-    wx.send_message("【标题测试】\n通知正文测试")
+	run_compare()
